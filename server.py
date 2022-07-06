9266b2f3e3dc6d5415d32f8d698ff139e18c94d4
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-07-06 14:05:09 +0800

    add: google api to fetch pages

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-from distutils.log import debug
-from flask import Flask
+from flask import request, jsonify, Flask
+import requests
 
 app = Flask(__name__)
+
+API_KEY = "AIzaSyC5FUhXQK-HirDBHAPE5kiG6g20FRLF4Z4"
+SEARCH_ENGINE_ID = 'edfc0e73a51df8fbd'
+GOOGLE_API = ' https://www.googleapis.com/customsearch/v1?key=' + \
+    API_KEY+'&cx='+SEARCH_ENGINE_ID
 
 # Members API Route
 
@@ -9,6 +14,22 @@ app = Flask(__name__)
 @app.route("/choices")
 def choices():
     return {"choices": ["choice1", "choice2", "choice3"]}
+
+
+@app.route("/pages", methods=["GET"])
+def pages():
+
+    if 'choice' in request.args:
+        choice = str(request.args['choice'])
+
+    if 'lvl' in request.args:
+        lvl = str(request.args['lvl'])
+
+    results = []
+
+    url = GOOGLE_API+'&q='+lvl+"%20"+choice
+    response = requests.get(url)
+    return response.json()
 
 
 if __name__ == "__main__":
